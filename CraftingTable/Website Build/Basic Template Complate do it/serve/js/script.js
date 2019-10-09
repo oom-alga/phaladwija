0a07b4e6892ed59084819fbfcab071066dc73c6e
oom-alga <sosial.alga@gmail.com>
2019-10-09 23:14:11 +0700

    09/10/2019 23:14

--- CraftingTable/Website Build/Basic Template Complate do it/serve/js/script.js
+++ CraftingTable/Website Build/Basic Template Complate do it/serve/js/script.js
@@ -1,74 +1,82 @@
 initBarba();
 
-var html = document.documentElement;
-var body = document.body;
+// SmoothScroll
+// new SmoothScroll(document,50,20)
+new SmoothScroll(document,50,15)
+function SmoothScroll(target, speed, smooth) {
+	if (target === document)
+		target = (document.scrollingElement 
+              || document.documentElement 
+              || document.body.parentNode 
+              || document.body)
+      
+	var moving = false
+	var pos = target.scrollTop
+  var frame = target === document.body 
+              && document.documentElement 
+              ? document.documentElement 
+              : target
+  
+	target.addEventListener('mousewheel', scrolled, { passive: false })
+	target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
 
-var scroller = {
-  target: document.querySelector("#scroll-container"),
-  ease: 0.05, // <= scroll speed
-  endY: 0,
-  y: 0,
-  resizeRequest: 1,
-  scrollRequest: 0,
+	function scrolled(e) {
+		e.preventDefault();
+		var delta = normalizeWheelDelta(e)
+		pos += -delta * speed
+		pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight))
+		if (!moving) update()
+	}
+
+	function normalizeWheelDelta(e){
+		if(e.detail){
+			if(e.wheelDelta)
+				return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1)
+			else
+				return -e.detail/3
+		}else
+			return e.wheelDelta/120
+	}
+
+	function update() {
+		moving = true
+		var delta = (pos - target.scrollTop) / smooth
+		target.scrollTop += delta
+		if (Math.abs(delta) > 0.5)
+			requestFrame(update)
+		else
+			moving = false
+  }
+  
+	var requestFrame = function() {
+		return (
+			window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.oRequestAnimationFrame ||
+			window.msRequestAnimationFrame ||
+			function(func) {
+				window.setTimeout(func, 1000 / 50);
+			}
+		);
+	}()
+}
+
+
+// progressBarScroll
+function progressBarScroll() {
+  let winScroll   = document.body.scrollTop || document.documentElement.scrollTop,
+      height      = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+      scrolled    = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.height = scrolled + "%";
+}
+
+window.onscroll = function () {
+  progressBarScroll();
 };
 
-var requestId = null;
 
-TweenLite.set(scroller.target, {
-  rotation: 0.01,
-  force3D: true
-});
-
-window.addEventListener("load", onLoad);
-
-function onLoad() {    
-  updateScroller();  
-  window.focus();
-  window.addEventListener("resize", onResize);
-  document.addEventListener("scroll", onScroll); 
-}
-
-function updateScroller() {
-  
-  var resized = scroller.resizeRequest > 0;
-    
-  if (resized) {    
-    var height = scroller.target.clientHeight;
-    body.style.height = height + "px";
-    scroller.resizeRequest = 0;
-  }
-      
-  var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
-
-  scroller.endY = scrollY;
-  scroller.y += (scrollY - scroller.y) * scroller.ease;
-
-  if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-    scroller.y = scrollY;
-    scroller.scrollRequest = 0;
-  }
-  
-  TweenLite.set(scroller.target, { 
-    y: -scroller.y 
-  });
-  
-  requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
-}
-
-function onScroll() {
-  scroller.scrollRequest++;
-  if (!requestId) {
-    requestId = requestAnimationFrame(updateScroller);
-  }
-}
-
-function onResize() {
-  scroller.resizeRequest++;
-  if (!requestId) {
-    requestId = requestAnimationFrame(updateScroller);
-  }
-}
-
+// owl Slider
 function owl() {
   $('.owl-carousel-basic').owlCarousel({
     loop:true,
@@ -84,30 +92,44 @@ function owl() {
     }
   });
 }
+
+
+// scrollMagic
 function scrollMagic() {
   var ctrl = new ScrollMagic.Controller();
   $(".items-scroll").each(function(i) {
     var inner = $(this).find(".inner");
     var outer = $(this).find(".outer");
     var tl = new TimelineMax();
-    
     tl.from(outer, 0.25, { scaleX: 0 });
     tl.from(inner, 0.65, { yPercent: 100, ease: Back.easeOut });
-    
     new ScrollMagic.Scene({
       triggerElement: this,
       triggerHook: 0.15
     })
       .setTween(tl)
+      // .addIndicators() 
+      .addTo(ctrl);
+  });
+  $(".wrap-parallax").each(function(i) {
+    var outer = $(this).find('.background');
+    var tk = new TimelineMax();
+    tk.from(outer, 0.5, {scaleX: 0, ease: Expo.easeOut});
+    new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.75
+    })
+      .setTween(tk)
       .addIndicators() 
       .addTo(ctrl);
   });
-  
 }
+
 function handleAnimations() {
   var Homepage = Barba.BaseView.extend({
     namespace: 'homepage',
     onEnter: function() {
+      parallax();
       console.log("onEnter");
     },
     onEnterCompleted: function() {
@@ -132,7 +154,6 @@ function handleAnimations() {
     },
     onLeave: function() {
       console.log("onLeave");
-     
     },
     onLeaveCompleted: function() {
       console.log("onLeaveCompleted");
@@ -143,6 +164,7 @@ function handleAnimations() {
     namespace: 'ScrollMagic',
     onEnter: function() {
       scrollMagic();
+      parallax();
       console.log("onEnter ScrollMagic");
     },
     onEnterCompleted: function() {
@@ -161,6 +183,9 @@ function handleAnimations() {
   ScrollMagic.init();
 
 }
+
+
+// Barba
 function initBarba() {
   var FadeTransition = Barba.BaseTransition.extend({
       start: function(){
@@ -185,11 +210,29 @@ function initBarba() {
         });
       }
   });
-
   Barba.Pjax.getTransition = function() {
       return FadeTransition;
   };
+
   handleAnimations();
   Barba.Pjax.cacheEnabled = false;
   Barba.Pjax.start();
+}
+
+
+
+// parallax image
+function parallax(){
+  $.fn.parallax = function(resistance, mouse) {
+    $el = $(this);
+    TweenLite.to($el, 0.2, {
+      x: -((mouse.clientX - window.innerWidth / 2) / resistance),
+      y: -((mouse.clientY - window.innerHeight / 2) / resistance)
+    });
+  };
+  $(".wrap-parallax").mousemove(function(e) {
+    $(".background", this).parallax(-10, e);
+    $(".title", this).parallax(10, e);
+    $(".desc", this).parallax(15, e);
+  });
 }
