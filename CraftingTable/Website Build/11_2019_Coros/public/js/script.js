ac3459c0fceae974d54ec3e1cbc9af43501ca0f5
oom-alga <sosial.alga@gmail.com>
2019-12-08 22:07:01 +0700

    rumah 08/12/2019 22:06

--- CraftingTable/Website Build/11_2019_Coros/public/js/script.js
+++ CraftingTable/Website Build/11_2019_Coros/public/js/script.js
@@ -1,86 +1,8 @@
-initBarba();
-
-// SmoothScroll
-// new SmoothScroll(document,50,20)
-
-
-$(".textBigTitle").fitText(0.97);
-$(".textTitle").fitText(0.3);
-
-new SmoothScroll(document,50,15)
-function SmoothScroll(target, speed, smooth) {
-	if (target === document)
-		target = (document.scrollingElement 
-              || document.documentElement 
-              || document.body.parentNode 
-              || document.body)
-      
-	var moving = false
-	var pos = target.scrollTop
-  var frame = target === document.body 
-              && document.documentElement 
-              ? document.documentElement 
-              : target
-  
-	target.addEventListener('mousewheel', scrolled, { passive: false })
-	target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
-
-	function scrolled(e) {
-		e.preventDefault();
-		var delta = normalizeWheelDelta(e)
-		pos += -delta * speed
-		pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight))
-		if (!moving) update()
-	}
-
-	function normalizeWheelDelta(e){
-		if(e.detail){
-			if(e.wheelDelta)
-				return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1)
-			else
-				return -e.detail/3
-		}else
-			return e.wheelDelta/120
-	}
-
-	function update() {
-		moving = true
-		var delta = (pos - target.scrollTop) / smooth
-		target.scrollTop += delta
-		if (Math.abs(delta) > 0.5)
-			requestFrame(update)
-		else
-			moving = false
-  }
-  
-	var requestFrame = function() {
-		return (
-			window.requestAnimationFrame ||
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame ||
-			window.oRequestAnimationFrame ||
-			window.msRequestAnimationFrame ||
-			function(func) {
-				window.setTimeout(func, 1000 / 50);
-			}
-		);
-	}()
-}
-
-
-// progressBarScroll
-function progressBarScroll() {
-  let winScroll   = document.body.scrollTop || document.documentElement.scrollTop,
-      height      = document.documentElement.scrollHeight - document.documentElement.clientHeight,
-      scrolled    = (winScroll / height) * 100;
-  document.getElementById("progressBar").style.height = scrolled + "%";
-}
-
-window.onscroll = function () {
-  progressBarScroll();
-};
-
-
+owl();
+text();
+itemSlide();
+menus();
+parallax();
 // owl Slider
 function owl() {
   $('.owl-carousel-indexHeader').owlCarousel({
@@ -97,136 +19,36 @@ function owl() {
   });
 }
 
+function text(){
+  $(".textBigTitle").fitText(0.97);
+  $(".textTitle").fitText(0.3);
+}
 
-// scrollMagic
-function scrollMagic() {
-  var ctrl = new ScrollMagic.Controller();
-  $(".items-scroll").each(function(i) {
-    var inner = $(this).find(".inner");
-    var outer = $(this).find(".outer");
-    var tl = new TimelineMax();
-    tl.from(outer, 0.25, { scaleX: 0 });
-    tl.from(inner, 0.65, { yPercent: 100, ease: Back.easeOut });
-    new ScrollMagic.Scene({
-      triggerElement: this,
-      triggerHook: 0.15
-    })
-      .setTween(tl)
-      // .addIndicators() 
-      .addTo(ctrl);
-  });
-  $(".wrap-parallax").each(function(i) {
-    var outer = $(this).find('.background');
-    var tk = new TimelineMax();
-    tk.from(outer, 0.5, {scaleX: 0, ease: Expo.easeOut});
-    new ScrollMagic.Scene({
-      triggerElement: this,
-      triggerHook: 0.75
-    })
-      .setTween(tk)
-      .addIndicators() 
-      .addTo(ctrl);
+function itemSlide() {
+  var controller = new ScrollMagic.Controller();
+  var horizontalSlide = new TimelineMax()
+  // animate panels
+  .to("#itemSlide", 1,   {x: "-20%"})	
+  .to("#itemSlide", 1,   {x: "-40%"})
+  .to("#itemSlide", 1,   {x: "-60%"})
+  .to("#itemSlide", 1,   {x: "-80%"})
+
+  // create scene to pin and link animation
+  new ScrollMagic.Scene({triggerElement: "#projectItem.one",triggerHook: "onLeave",duration: "500%"})
+  .setPin("#projectItem.one").setTween(horizontalSlide).addIndicators({name: "1 (duration: 300)"}).addTo(controller);
+
+}
+ 
+function menus(){
+  $("nav.three h3").mouseenter(function() {
+    $this = $(this);
+    $("nav.three").css('background-color', function() {
+      return $this.data('bgcolor');
+    });
+    $("nav.three").css("background-image", "url(" + $(this).data("bg") + ")");
   });
 }
 
-function handleAnimations() {
-  var Homepage = Barba.BaseView.extend({
-    namespace: 'homepage',
-    onEnter: function() {
-      parallax();
-      owl();
-      console.log("onEnter");
-    },
-    onEnterCompleted: function() {
-      console.log("onEnterCompleted");
-    },
-    onLeave: function() {
-      console.log("onLeave");
-    },
-    onLeaveCompleted: function() {
-      console.log("onLeaveCompleted");
-    }
-  });
-
-  var OwlCarousel = Barba.BaseView.extend({
-    namespace: 'OwlCarousel',
-    onEnter: function() {
-      owl();
-      console.log("onEnter");
-    },
-    onEnterCompleted: function() {
-      console.log("onEnterCompleted");
-    },
-    onLeave: function() {
-      console.log("onLeave");
-    },
-    onLeaveCompleted: function() {
-      console.log("onLeaveCompleted");
-    }
-  });
-
-  var ScrollMagic = Barba.BaseView.extend({
-    namespace: 'ScrollMagic',
-    onEnter: function() {
-      scrollMagic();
-      parallax();
-      console.log("onEnter ScrollMagic");
-    },
-    onEnterCompleted: function() {
-      console.log("onEnterCompleted ScrollMagic");
-    },
-    onLeave: function() {
-      console.log("onLeave ScrollMagic");
-    },
-    onLeaveCompleted: function() {
-      console.log("onLeaveCompleted ScrollMagic");
-    }
-  });
-  
-  Homepage.init();
-  OwlCarousel.init();
-  ScrollMagic.init();
-
-}
-
-
-// Barba
-function initBarba() {
-  var FadeTransition = Barba.BaseTransition.extend({
-      start: function(){
-        console.log("slow");
-        this.newContainerLoading.then(val => this.finish($(this.newContainer)));
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        $(".load-page img").css({
-          "opacity": "1",
-        }, "slow");
-      },
-      finish: function(nc) {
-        nc.hide();
-        var _this = this;        
-        $(this.oldContainer).fadeOut(500).promise().done(() => { 
-          nc.css('visibility','visible');
-          nc.fadeIn(500, function(){
-              $(".load-page img").css({
-                "opacity": "0",
-              }, "slow");
-            _this.done();
-          })
-        });
-      }
-  });
-  Barba.Pjax.getTransition = function() {
-      return FadeTransition;
-  };
-
-  handleAnimations();
-  Barba.Pjax.cacheEnabled = false;
-  Barba.Pjax.start();
-}
-
-
-
-// parallax image
 function parallax(){
   $.fn.parallax = function(resistance, mouse) {
     $el = $(this);
@@ -241,38 +63,3 @@ function parallax(){
     $(".desc", this).parallax(15, e);
   });
 }
-
-
-$(function () { // wait for document ready
-  
-  var controller = new ScrollMagic.Controller();
-  
-  var horizontalSlide = new TimelineMax()
-  // animate panels
-  .to("#itemSlide", 1,   {x: "-20%"})	
-  .to("#itemSlide", 1,   {x: "-40%"})
-  .to("#itemSlide", 1,   {x: "-60%"})
-  .to("#itemSlide", 1,   {x: "-80%"})
-
-
-  // create scene to pin and link animation
-  new ScrollMagic.Scene({
-    triggerElement: "#projectItem",
-    triggerHook: "onLeave",
-    duration: "500%"
-  })
-    .setPin("#projectItem")
-    .setTween(horizontalSlide)
-    .addIndicators({name: "1 (duration: 300)"})
-    .addIndicators() // add indicators (requires plugin)
-    .addTo(controller);
-});
-
-
-$("#menus h3").hover(function() {
-	$this = $(this);
-	$("#menus").css('background-color', function() {
-		return $this.data('bgcolor');
-	});
-	$("#menus").css("background-image", "url(" + $(this).data("bg") + ")");
-});
---
+++ CraftingTable/Website Build/old/11_2019_Coros/gulpfile.js
@@ -0,0 +1,48 @@
+var gulp         = require("gulp");
+var browserSync  = require("browser-sync");
+var sass         = require("gulp-sass");
+var pug          = require('gulp-pug');
+
+gulp.task('sass', function () {
+    return gulp.src([
+        './serve/sass/**/*.scss',
+    ])
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js', function(){
+    return gulp.src("./serve/js/*.js")
+    .pipe(gulp.dest("./public/js"));
+});
+
+gulp.task('pug', function buildHTML() {
+  return gulp.src('./serve/pug/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./public'));
+});
+
+gulp.task("serve",function(){
+    browserSync.init({
+        server: {
+            baseDir: "./public/"
+        }
+    })
+    
+    // gulp.watch('./serve/sass/**/*.scss', gulp.series('sass')).on("change",browserSync.reload);
+    // gulp.watch('./serve/js/**/*.js', gulp.series('js')).on("change",browserSync.reload);
+    // gulp.watch("./serve/pug/**/*.pug", gulp.series('pug'));
+    
+
+    gulp.watch('./serve/sass/**/*.scss', gulp.series('sass'));
+    gulp.watch('./serve/js/**/*.js', gulp.series('js'));
+    gulp.watch("./serve/pug/**/*.pug", gulp.series('pug'));
+    gulp.watch("./serve/template/**/*.pug", gulp.series('pug'));
+
+    gulp.watch("./serve/js/*.js").on("change",browserSync.reload);
+    gulp.watch("./serve/sass/**/*.scss").on("change",browserSync.reload);
+    gulp.watch("./serve/pug/**/*.pug").on("change",browserSync.reload);
+    gulp.watch("./serve/template/**/*.pug").on("change",browserSync.reload);
+});
